c1737d293a7b8e25eb92620467c18b5bc67c7eac
jose4400 <jose4400@stud.kea.dk>
2023-08-06 14:18:06 +0200

    rettet igen

--- src/main.js
+++ src/main.js
@@ -3,3 +3,23 @@ import "./style.css";
 import Alpine from "alpinejs";
 window.Alpine = Alpine;
 Alpine.start();
+
+const burgerMenu = document.querySelector(".burger-menu");
+
+const navigationMenu = document.querySelector(".navigation");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenu.classList.toggle("active");
+  navigationMenu.classList.toggle("active");
+});
+
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
+});
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
@@ -14,9 +30,7 @@ module.exports = {
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  plugins: [
-    plugin(function ({ addUtilities, addComponents, e, config }) {
-      // Add your custom styles here
-    }),
-  ],
+  plugins: [],
 };
+
+
